e03ded1a80d4949c903ca367a678b3f9bf9f2a53
Emil <emilpet93@gmail.com>
2019-07-04 11:21:32 +0200

    Setup project and added searchbar component

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+class App extends React.Component {
+  state = {
+
+  };
+  render() {
+    return (
+      <div className="app">
+        <SearchBar />
+      </div>
+    )
+  }
+}
+
+export default App;
